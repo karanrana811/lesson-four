8778abcd229b9db0eb5ec7e9fa99a4ef6436f5a7
Karan <rkrana811@gmail.com>
2022-09-30 01:09:53 -0500

    Completed LessonFour
    Made the app. Need to style and beautify.

---
+++ app.js
@@ -0,0 +1,44 @@
+// var userName = prompt("Give me your name")
+// alert("Your name is " + userName)
+
+var btnTranslate = document.querySelector('#btn-translate');
+var textInput = document.querySelector('#txt-input'); 
+var outputDiv = document.querySelector('#output');
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function textArea() {
+    // textInput.innerText="This is changed!";
+    console.log(textInput.value);
+}
+
+function clickHandler() {
+    // outputDiv.innerText="You typed " + textInput.value
+    console.log(outputDiv.innerText)
+}
+
+function errorHandler(error) {
+    console.log("An error occured");
+    alert("An error occured. Please try again after an hour.");
+}
+
+
+
+function getTranslationURL(test) {
+    return serverURL + "?" + "text=" + test
+}
+
+function clickEventHandler() {
+    console.log("clicked");
+    clickHandler();
+    fetch(getTranslationURL(textInput.value))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        console.log(translatedText)
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
+
